406b7d7fe8fe4018a99cbef2a9b65202f8af8ec8
Eder Dias <37964092+EderOD@users.noreply.github.com>
2020-11-20 22:51:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask
+from subprocess import run, PIPE
+
+from flask import logging, Flask, render_template, request
+import os
+import numpy as np
+import librosa
+from joblib import load
 app = Flask(__name__)
 
 @app.route("/")
