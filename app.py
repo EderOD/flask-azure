75867c72331c5490081458f2241acb95f37171bc
Eder Dias <37964092+EderOD@users.noreply.github.com>
2020-11-21 02:44:43 -0300

    Revert "Update app.py"
    This reverts commit 5d6431eb2a4f97eecd55b37d4a20e9cca85983c1.

--- app.py
+++ app.py
@@ -1,29 +1,5 @@
 import flask
-import numpy as np
-import librosa
-from joblib import load
 app = flask.Flask(__name__)
-
-model = load("modelo.joblib")
-def predicao(p_nomeArquivo):
-
-    X, sample_rate = librosa.load(os.path.join(p_nomeArquivo), res_type='kaiser_fast')
-
-    stft=np.abs(librosa.stft(X))
-    atributos=np.array([])
-
-    mfccs=np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
-    atributos=np.hstack((atributos, mfccs))
-
-    chroma=np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
-    atributos=np.hstack((atributos, chroma))
-
-    mel=np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
-    atributos=np.hstack((atributos, mel))
-    x = []
-    x.append(atributos)
-    stri = model.predict(x)[0]
-    return stri
 
 @app.route('/')
 def index():
@@ -34,8 +10,7 @@ def index():
 def audio():
     with open('audio.wav', 'wb') as f:
         f.write(flask.request.data)
-    previsao = str(predicao('audio.wav'))
-    return previsao
+    return 'audio gravado'
 
 
 if __name__ == "__main__":
